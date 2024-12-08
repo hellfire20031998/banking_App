1dc485f78095cbd758537d1e644f946d61ae4216
hellfire20031998 <166204348+hellfire20031998@users.noreply.github.com>
2024-12-08 20:22:53 +0530

    Initial commit

---
+++ src/test/java/com/hellFire/banking_App/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellFire.banking_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
